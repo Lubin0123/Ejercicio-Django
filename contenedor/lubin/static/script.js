fd822c57e8e052160bad4a2e444d36331d8504ef
LAPTOP-VQL2OFUQ\Lubin Reyes <lubinstevenr@gmail.com>
2024-04-26 16:51:59 -0500

    ya se encuentra en funcionamiento la zona de administrador con un usuario preparado para trabajar

---
+++ contenedor/lubin/static/script.js
@@ -0,0 +1,2 @@
+console.log("Hola mis chinos")
+// el metodo console.log imprimira el mensaje en la consola del cliente que en este caso seria el navegador
--- contenedor/lubin/views.py
+++ contenedor/lubin/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def indexView(request):
+      '''esta es la pagina principal'''
+      return render(request, "index.html")
